f13644b3d1dfe5de45378e1979ff783fae4c8f0f
eskimo220 <eskimo220@DESKTOP-FK1AHIT>
2024-02-07 23:01:27 +0800

    2/7

--- client/start.sh
+++ client/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir /usr/local/share/ca-certificates/extra/
-sleep 3
-curl --proxy mitmproxy:8080 -o /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
-update-ca-certificates
-
 ip route del default
 ip route add default via 10.121.0.2
+
+mkdir /usr/local/share/ca-certificates/extra/
+sleep 3
+curl -o /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
+update-ca-certificates
 
 # 执行原始容器的命令
 exec "$@"
