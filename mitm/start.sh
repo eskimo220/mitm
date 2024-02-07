e9c3ee8e1bbdcb449aea788ff4246cc15e6c2cac
wanghaoqi <eskimo220@sina.com>
2024-02-07 17:11:11 +0800

    NAT

--- mitm/start.sh
+++ mitm/start.sh
@@ -2,6 +2,7 @@
 
 # 在这里添加您想在启动时执行的命令
 # 例如，定义 iptables 规则
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
 
