a5a1216d36178a6363a94bdc06b0148dbc37e492
wanghaoqi <eskimo220@sina.com>
2024-02-07 14:16:41 +0800

    Certificates

--- client/start.sh
+++ client/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl --proxy mitmproxy:8080 -o /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
+update-ca-certificates
+
 ip route del default
 ip route add default via 10.121.0.2
 
