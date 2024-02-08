a08512379f71767e27bec8dd6007cf199c8160c8
wanghaoqi <eskimo220@sina.com>
2024-02-08 11:21:58 +0800

    remove extra

--- client/start.sh
+++ client/start.sh
@@ -3,9 +3,8 @@
 ip route del default
 ip route add default via 10.121.0.2
 
-mkdir /usr/local/share/ca-certificates/extra/
 sleep 5
-curl -o /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
+curl -o /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
 update-ca-certificates
 
 # 执行原始容器的命令
