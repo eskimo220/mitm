b79ba617eea4f552f9a2ef2db9788ee161c64a2b
wanghaoqi <eskimo220@sina.com>
2024-02-08 11:02:51 +0800

    sleep 5

--- client/start.sh
+++ client/start.sh
@@ -4,7 +4,7 @@ ip route del default
 ip route add default via 10.121.0.2
 
 mkdir /usr/local/share/ca-certificates/extra/
-sleep 3
+sleep 5
 curl -o /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
 update-ca-certificates
 
--- pandora/start.sh
+++ pandora/start.sh
@@ -3,7 +3,7 @@
 ip route del default
 ip route add default via 10.121.0.2
 
-sleep 3
+sleep 5
 curl -o /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt http://mitm.it/cert/pem
 update-ca-certificates
 
